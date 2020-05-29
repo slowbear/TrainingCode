baf7355a5fcef0d8e450a01c9a8e986ab3753938
slowbear <slowyfine@gmail.com>
2020-05-29 13:47:18 +0800

    Update "E" solution.

--- Codeforces/Educational Round 88/E.cpp
+++ Codeforces/Educational Round 88/E.cpp
@@ -1,4 +1,3 @@
-#error unfinished
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
@@ -12,11 +11,21 @@ constexpr int p = 998244353;
 
 constexpr int MAX_N = 501000;
 
-int f[20][20];
+int ni[MAX_N];
+
+long long C[MAX_N];
 
 int main() {
   int n, k;
   scanf("%d%d", &n, &k);
-  memset(f, 0, sizeof(f));
-  f[0][1] = 1;
+  ni[1] = 1;
+  for (int i = 2; i < MAX_N; ++i) ni[i] = 1LL * (p - p / i) * ni[p % i] % p;
+  long long ans = 0;
+  memset(C, 0, sizeof(C));
+  C[k - 1] = 1;
+  for (int i = k; i < n; ++i) C[i] = C[i - 1] * i % p * ni[i - k + 1] % p;
+  for (int i = 1; i <= n; ++i) {
+    ans = (ans + C[n / i - 1]) % p;
+  }
+  printf("%lld\n", ans);
 }
