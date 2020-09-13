905e4558237ef9b2f3db4eec14529668f93c5873
slowbear <slowyfine@gmail.com>
2020-09-13 12:41:37 +0800

    Update header template.

--- Template/header.h
+++ Template/header.h
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 using LL = long long;
+using Pii = pair<int, int>;
+using Pll = pair<LL, LL>;
 using VI = vector<int>;
 using VP = vector<pair<int, int>>;
-#define rep(i, a, b) for (auto i = (a); i != (b); ++i)
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define rev(i, a, b) for (auto i = (b - 1); i >= (a); --i)
 #define grep(i, u) for (auto i = gh[u]; i != -1; i = gn[i])
 #define mem(x, v) memset(x, v, sizeof(x))
 #define cpy(x, y) memcpy(x, y, sizeof(x))
